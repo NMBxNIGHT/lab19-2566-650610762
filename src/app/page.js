14ee34150f46af0cf2bda2d577fc600cfb9dd8d3
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-09-18 15:09:07 +0700

    fix login button and mycourse loading status

--- src/app/page.js
+++ src/app/page.js
@@ -36,10 +36,12 @@ export default function Home() {
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true);
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
     });
     setMyCourses(resp.data.courses);
+    setLoadingMyCourses(false);
   };
 
   useEffect(() => {
@@ -54,15 +56,19 @@ export default function Home() {
 
   const login = async () => {
     try {
+      setLoadingLogin(true);
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      setLoadingLogin(false);
     } catch (error) {
+      setLoadingLogin(true);
       if (error.response.data) {
         alert(error.response.data.message);
       }
+      setLoadingLogin(false);
     }
   };
 
@@ -105,7 +111,14 @@ export default function Home() {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              {/* disabled login button  */}
+              {loadingLogin ? (
+                <Button onClick={login} disabled={true}>
+                  Login...
+                </Button>
+              ) : (
+                <Button onClick={login}>Login</Button>
+              )}
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +146,14 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+          {/* Loading dots */}
+          {loadingMyCourses && <Loader variant="dots" />}
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year="2023"
+          fullName="Thiranat Kakanmee"
+          studentId="650610762"
+        />
       </Stack>
     </Container>
   );
